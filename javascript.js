03f8825ca769efd4194bfd3ac7150c9e30b83e9b
Danaa4 <Danaa4@users.noreply.github.com>
2021-01-03 01:34:06 +0300

    done

--- javascript.js
+++ javascript.js
@@ -13,4 +13,34 @@ let numberOfYears = 2021-1969
 
 console.log(`🌑هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر${numberOfYears}`)
 
+// السؤال الثالث
+function submit() {
 
+let q =parseInt(document.getElementById("Quizes").value)
+let m =parseInt(document.getElementById("Midterm").value)
+let f =parseInt(document.getElementById("FinalTest").value)
+let o =parseInt(document.getElementById("Oral").value)
+
+let total=q+m+f+o
+
+if(total<=100 && total>=90 ){
+    document.getElementById("total_grades").innerText =total
+    document.getElementById("final_grade").innerText =`A`
+  }
+  else if(total>=80){
+   document.getElementById("total_grades").innerText =total
+   document.getElementById("final_grade").innerText =`B`
+  }
+  else if(total>=70){
+   document.getElementById("total_grades").innerText =total
+   document.getElementById("final_grade").innerText =`C`
+  }
+  else if(total>=60){
+    document.getElementById("total_grades").innerText =total
+    document.getElementById("final_grade").innerText =`D`
+  }
+  else{
+   document.getElementById("total_grades").innerText =total
+   document.getElementById("final_grade").innerText =`F`
+  }
+}
